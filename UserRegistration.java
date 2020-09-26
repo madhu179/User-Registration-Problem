72a81514447067dbd958f0330e8f48f469fdaa1b
madhu179 <madhubabu232343@gmail.com>
2020-09-26 14:10:00 +0530

    Commiting use case 2 files

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,10 +5,12 @@ public class UserRegistration
 	public static void main(String[] args)
 	{
 	  Scanner sc = new Scanner(System.in);
-	  int i=1;
+	  int j=0,c1=1,c2=1;
 	  while(true)
 	  {
-	  	  if(i==1)
+	  	if(j==0)
+	  	{
+	  	  if(c1==1)
 	  	  {
 	      System.out.println("Enter the First Name");
 	      }
@@ -22,9 +24,32 @@ public class UserRegistration
 	      Matcher m = patrn.matcher(userName);
 	      if(m.matches())
 	      {
+	      	j+=1;
+	      }
+	    c1+=1;
+  	    }
+
+	  	if(j==1)
+	  	{
+          if(c2==1)
+	  	  {
+	      System.out.println("Enter the Last Name");
+	      }
+	      else
+	      {
+	      	System.out.println("Enter a valid Last Name");
+	      }
+	      String userName = sc.nextLine();
+	      String regex = "[A-Z]{1}[a-z]{2,}"; 
+	      Pattern patrn = Pattern.compile(regex);
+	      Matcher m = patrn.matcher(userName);
+	      if(m.matches())
+	      {
+	      	j+=1;
 	      	break;
 	      }
-	    i+=1;
-  	  }
+	    c2+=1;
+	  	}
+  	}
 	}
 }
