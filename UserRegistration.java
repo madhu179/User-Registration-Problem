89cae0543c44f76015994a7aaff72d53b72123b7
madhu179 <madhubabu232343@gmail.com>
2020-09-26 14:39:08 +0530

    Commiting use case 6 files

--- UserRegistration.java
+++ UserRegistration.java
@@ -106,7 +106,7 @@ public class UserRegistration
 	      	System.out.println("Enter a valid Password");
 	      }
 	      String userName = sc.nextLine();
-	      String regex = ".{8,}"; 
+	      String regex = "(?=.*[A-Z]).{8,}"; 
 	      Pattern patrn = Pattern.compile(regex);
 	      Matcher m = patrn.matcher(userName);
 	      if(m.matches())
